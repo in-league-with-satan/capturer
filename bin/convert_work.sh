c63a1361111c132cade3dbb73632d933efec70a6
Андрей Чепрасов <ae.cheprasov@gmail.com>
2017-01-29 18:11:35 +0300

    в скрипт конвертера добавлена опция для использования libx265

--- bin/convert_work.sh
+++ bin/convert_work.sh
@@ -3,23 +3,30 @@
 filename=$(basename $1)
 
 ff_bin="ffmpeg"
+v_enc="libx264"
+pix_fmt=""
 ch_map=""
 preset="slow"
 crf=18
 
-if (( $(./ffprobe -v 0 -of compact=p=0 -select_streams 0 -show_entries stream=width $1) > 1920 )); then
-    preset="medium"
-    crf=20
-fi
+#if (( $(./ffprobe -v 0 -of compact=p=0 -select_streams 0 -show_entries stream=width $1) > 1920 )); then
+#    preset="medium"
+#    crf=20
+#fi
 
 for i in "$@"
 do
     if [ "${i}" == "--10bit" ]; then
         ff_bin="ffmpeg_10bit"
+        pix_fmt="-pix_fmt yuv444p10le"
+    fi
+
+    if [ "${i}" == "--x265" ]; then
+        v_enc="libx265"
     fi
 
     if [ "${i}" == "--remap_ac" ]; then
-#                             l     r     c    lfe   bl    br     sl    sr
+#                             l     r     c    lfe   bl    br    sl    sr
         ch_map="-af pan=7.1|c0=c0|c1=c1|c2=c3|c3=c2|c4=c6|c5=c7|c6=c4|c7=c5"
     fi
 
@@ -28,9 +35,8 @@ do
     fi
 done
 
-
 ./$ff_bin -i videos/$filename -async 1 \
-        -map 0:v -c:v libx264 -crf $crf -preset $preset -vlevel 5.1 \
+        -map 0:v -c:v $v_enc -crf $crf -preset $preset $pix_fmt -vlevel 5.1 \
         $ch_map \
         -map 0:a -c:a:0 flac -compression_level 6 \
         -map 0:a -c:a:1 libvorbis -ac:2 6 -aq 6 \
