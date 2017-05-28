1d75b9b0e710e303949e7fd19c18fc2ca97ebdf3
Андрей Чепрасов <ae.cheprasov@gmail.com>
2017-05-28 04:15:38 +0300

    обновлен скрипт сборки ffmpeg   - добавлен пакет nasm - libx264 теперь требует его для сборки   - добавлен пакет numa - нужен для libx265. хотя собирается и без него, но на всякий случай пусть будет   - репозитории теперь обновляются через git pull

--- externals/3rdparty/ffmpeg/build.sh
+++ externals/3rdparty/ffmpeg/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #sudo apt-get -y install autoconf automake libtool build-essential cmake git mercurial
-
+#cpan Font::TTF::Font
+#cpan Sort::Versions
 
 str_opt="-march=native -O3"
 
@@ -30,9 +31,13 @@ fi
 
 cd $PATH_BUILD
 if [ ! -e yasm ]; then
-  git clone --depth 1 git://github.com/yasm/yasm.git
+  git clone git://github.com/yasm/yasm.git
+  cd yasm
+else
+  cd yasm
+  git reset --hard
+  git pull
 fi
-cd yasm
 autoreconf -fiv
 CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --bindir="$PATH_BASE/bin"
 make -j$cpu_count
@@ -41,10 +46,44 @@ make install
 
 
 cd $PATH_BUILD
-if [ ! -e x264 ]; then
-  git clone --depth 1 git://git.videolan.org/x264.git
+if [ ! -e nasm ]; then
+  git clone git://repo.or.cz/nasm.git
+  cd nasm
+else
+  cd nasm
+  git reset --hard
+  git pull
 fi
-cd x264
+autoreconf -fiv
+CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --bindir="$PATH_BASE/bin"
+make everything -j$cpu_count
+make install
+
+
+cd $PATH_BUILD
+if [ ! -e numactl ]; then
+  git clone https://github.com/numactl/numactl
+  cd numactl
+else
+  cd numactl
+  git reset --hard
+  git pull
+fi
+autoreconf -fiv
+CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --bindir="$PATH_BASE/bin"
+make -j$cpu_count
+make install
+
+
+cd $PATH_BUILD
+if [ ! -e x264 ]; then
+  git clone git://git.videolan.org/x264.git
+  cd x264
+else
+  cd x264
+  git reset --hard
+  git pull
+fi
 make distclean
 ./configure --prefix="$PATH_BASE" --bindir="$PATH_BASE/bin" --enable-static --extra-cflags="$str_opt"
 make -j$cpu_count
@@ -55,8 +94,13 @@ make install
 cd $PATH_BUILD
 if [ ! -e x265 ]; then
   hg clone https://bitbucket.org/multicoreware/x265
+  cd x265/build/linux
+else
+  cd x265
+  hg update --clean
+  hg pull
+  cd build/linux
 fi
-cd $PATH_BUILD/x265/build/linux
 make clean
 export CFLAGS="$str_opt"
 cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$PATH_BASE" -DENABLE_SHARED:bool=off ../../source
@@ -67,8 +111,12 @@ make install
 cd $PATH_BUILD
 if [ ! -e fdk-aac ]; then
   git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
+  cd fdk-aac
+else
+  cd fdk-aac
+  git reset --hard
+  git pull
 fi
-cd fdk-aac
 autoreconf -fiv
 CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --disable-shared CFLAGS="$str_opt"
 make -j$cpu_count
@@ -79,6 +127,8 @@ make install
 cd $PATH_BUILD
 if [ ! -e lame-3.99.5.tar.gz ]; then
   wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
+fi
+if [ ! -e lame-3.99.5 ]; then
   tar xzvf lame-3.99.5.tar.gz
 fi
 cd lame-3.99.5
@@ -91,8 +141,12 @@ make install
 cd $PATH_BUILD
 if [ ! -e opus ]; then
   git clone git://git.opus-codec.org/opus.git
+  cd opus
+else
+  cd opus
+  git reset --hard
+  git pull
 fi
-cd opus
 autoreconf -fiv
 CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --disable-shared CFLAGS="$str_opt"
 make -j$cpu_count
@@ -103,6 +157,8 @@ make install
 cd $PATH_BUILD
 if [ ! -e libogg-1.3.2.tar.gz ]; then
   wget http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
+fi
+if [ ! -e libogg-1.3.2 ]; then
   tar xzvf libogg-1.3.2.tar.gz
 fi
 cd libogg-1.3.2
@@ -115,6 +171,8 @@ make install
 cd $PATH_BUILD
 if [ ! -e libvorbis-1.3.5.tar.gz ]; then
   wget http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz
+fi
+if [ ! -e libvorbis-1.3.5 ]; then
   tar xzvf libvorbis-1.3.5.tar.gz
 fi
 cd libvorbis-1.3.5
@@ -125,8 +183,10 @@ make install
 
 
 cd $PATH_BUILD
-if [ ! -e speex-1.2.0 ]; then
+if [ ! -e speex-1.2.0.tar.gz ]; then
     wget http://downloads.us.xiph.org/releases/speex/speex-1.2.0.tar.gz
+fi
+if [ ! -e speex-1.2.0 ]; then
     tar xzvf speex-1.2.0.tar.gz
 fi
 cd speex-1.2.0
@@ -139,8 +199,12 @@ make install
 cd $PATH_BUILD
 if [ ! -e libvpx ]; then
   git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
+  cd libvpx
+else
+  cd libvpx
+  git reset --hard
+  git pull
 fi
-cd libvpx
 ./configure --prefix="$PATH_BASE" --disable-examples
 make -j$cpu_count
 make install
@@ -149,10 +213,13 @@ make install
 
 cd $PATH_BUILD
 if [ ! -e ffmpeg ]; then
-  git clone --depth 1 git://source.ffmpeg.org/ffmpeg
+  git clone git://source.ffmpeg.org/ffmpeg
 #  git clone --depth 1 https://git.ffmpeg.org/ffmpeg.git
+else
+  cd ffmpeg
+  git reset --hard
+  git pull
 fi
-cd ffmpeg
 make distclean
 ./configure --prefix="$PATH_BASE" --extra-cflags="-I$PATH_BASE/include -I$DECKLINK_INCLUDE $str_opt" --extra-ldflags="-L$PATH_BASE/lib" --bindir="$PATH_BASE/bin" --pkg-config-flags="--static" \
   --enable-gpl \
@@ -182,11 +249,21 @@ export PKG_CONFIG_PATH="$PATH_BASE/lib/pkgconfig"
 
 cd $PATH_BUILD
 cd yasm
+CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --bindir="$PATH_BASE/bin"
+make install
+
+
+cd $PATH_BUILD
+cd nasm
+CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --bindir="$PATH_BASE/bin"
+make install
+
+
+cd $PATH_BUILD
+cd numactl
 autoreconf -fiv
 CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --bindir="$PATH_BASE/bin"
-make -j$cpu_count
 make install
-#make distclean
 
 
 cd $PATH_BUILD
@@ -197,12 +274,11 @@ make -j$cpu_count
 make install
 
 
-cd $PATH_BUILD
 cd $PATH_BUILD/x265/build/linux
-make clean
-export CFLAGS="$str_opt"
+#make clean
+#export CFLAGS="$str_opt"
 cmake -G "Unix Makefiles" -DHIGH_BIT_DEPTH=ON -DMAIN12=OFF -DCMAKE_INSTALL_PREFIX="$PATH_BASE" -DENABLE_SHARED:bool=off ../../source
-make -j$cpu_count
+#make -j$cpu_count
 make install
 
 
@@ -210,58 +286,44 @@ cd $PATH_BUILD
 cd fdk-aac
 autoreconf -fiv
 CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --disable-shared CFLAGS="$str_opt"
-make -j$cpu_count
 make install
-#make distclean
 
 
 cd $PATH_BUILD
 cd lame-3.99.5
 CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --bindir="$PATH_BASE/bin" --disable-shared --enable-nasm CFLAGS="$str_opt"
-make -j$cpu_count
 make install
-#make distclean
 
 
 cd $PATH_BUILD
 cd opus
 autoreconf -fiv
 CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --disable-shared CFLAGS="$str_opt"
-make -j$cpu_count
 make install
-#make distclean
 
 
 cd $PATH_BUILD
 cd libogg-1.3.2
 CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --disable-shared CFLAGS="$str_opt"
-make -j$cpu_count
 make install
-#make distclean
 
 
 cd $PATH_BUILD
 cd libvorbis-1.3.5
 CFLAGS="$str_opt" LDFLAGS="-L$PATH_BASE/lib" CPPFLAGS="-I$PATH_BASE/include" ./configure --prefix="$PATH_BASE" --with-ogg="$PATH_BASE" --disable-shared CFLAGS="$str_opt"
-make -j$cpu_count
 make install
-#make distclean
 
 
 cd $PATH_BUILD
 cd speex-1.2.0
 CFLAGS="$str_opt" ./configure --prefix="$PATH_BASE" --disable-shared CFLAGS="$str_opt"
-make -j$cpu_count
 make install
-#make distclean
 
 
 cd $PATH_BUILD
 cd libvpx
 ./configure --prefix="$PATH_BASE" --disable-examples
-make -j$cpu_count
 make install
-#make clean
 
 
 cd $PATH_BUILD
