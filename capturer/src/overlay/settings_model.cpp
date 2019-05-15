3edf9540349a960f7395963dacda08c3e7f7cfb9
Андрей Чепрасов <ae.cheprasov@gmail.com>
2019-05-15 03:48:02 +0300

    обновлен копирайт

--- capturer/src/overlay/settings_model.cpp
+++ capturer/src/overlay/settings_model.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
 
-Copyright © 2018 Andrey Cheprasov <ae.cheprasov@gmail.com>
+Copyright © 2018-2019 Andrey Cheprasov <ae.cheprasov@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
